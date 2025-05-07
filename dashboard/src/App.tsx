e2e4e275824f139e98b5d191815a1bc646a851d2
Max A <corp.workMA@gmail.com>
2025-05-07 20:57:36 +0300

    test commit

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,13 +1,12 @@
-import { capitalizeFirstLetter } from "@pet/utils";
+import { capitalizeFirstLetter } from '@pet/utils'
 
 function App() {
-  const title = capitalizeFirstLetter("test my script");
-
+  const title = capitalizeFirstLetter('test my script')
   return (
     <>
       <p>{title}</p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- scripts/global_husky.mjs
+++ scripts/global_husky.mjs
@@ -1,7 +1,8 @@
 import fs from "node:fs";
+import { exec } from "node:child_process";
 
-const globalDirectory = process.cwd();
-const systemDirectories = ["node_modules", "scripts", "packages"];
+const systemDirectories = ["node_modules", "scripts"];
+const PACKAGES_DIRECTORY = "packages";
 
 const getDirectories = (source) =>
   fs
@@ -12,4 +13,45 @@ const getDirectories = (source) =>
       (name) => !name.startsWith(".") && !systemDirectories.includes(name)
     );
 
-console.log(getDirectories(globalDirectory));
+const getPackageName = (source, dir) => {
+  try {
+    const data = fs.readFileSync(`${source}/${dir}/package.json`, "utf-8");
+    const packageData = JSON.parse(data);
+    return packageData.name;
+  } catch (error) {
+    console.error(`Error for ${source}/${dir} - ${error}`);
+  }
+};
+
+const runScript = (packageName) => {
+  exec(`pnpm --filter ${packageName} pre-commit`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`Stderr: ${stderr}`);
+      return;
+    }
+    console.log(`Output:\n${stdout}`);
+  });
+};
+
+const getAllPackagesName = () => {};
+
+const main = () => {
+  const globalDirectory = process.cwd();
+  const apps = getDirectories(globalDirectory).filter(
+    (item) => item !== PACKAGES_DIRECTORY
+  );
+  const packagesDirectories = getDirectories(
+    `${globalDirectory}/${PACKAGES_DIRECTORY}`
+  );
+
+  for (let dir of [...apps, packagesDirectories]) {
+    const packageName = getPackageName(globalDirectory, dir);
+    runScript(packageName);
+  }
+};
+
+main();
