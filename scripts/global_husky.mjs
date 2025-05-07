db4b34cbdb4fda3d5c92f4b4fd3332d0902a9ae2
Max A <corp.workMA@gmail.com>
2025-05-07 09:53:08 +0300

    [dev-0005]: configure global husky

---
+++ scripts/global_husky.mjs
@@ -0,0 +1,15 @@
+import fs from "node:fs";
+
+const globalDirectory = process.cwd();
+const systemDirectories = ["node_modules", "scripts"];
+
+const getDirectories = (source) =>
+  fs
+    .readdirSync(source, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name)
+    .filter(
+      (name) => !name.startsWith(".") && !systemDirectories.includes(name)
+    );
+
+console.log(getDirectories(globalDirectory));
