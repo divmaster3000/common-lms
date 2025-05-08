b8b5699841888830cb585be72b80c9100e87599c
MaxAlieksieiev <65343519+divmaster3000@users.noreply.github.com>
2025-05-08 21:05:57 +0300

    [dev-0008]: add custom component (#8)

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,10 +1,15 @@
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 import { capitalizeFirstLetter } from '@pet/utils'
+import { Button } from 'ui'
 
 function App() {
   const title = capitalizeFirstLetter('test my script')
   return (
     <>
       <p>{title}</p>
+      <ChakraProvider value={defaultSystem}>
+        <Button onClick={() => alert('Clicked!')}>Hello from UI</Button>
+      </ChakraProvider>
     </>
   )
 }
