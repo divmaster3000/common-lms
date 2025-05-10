2dfa47fa6e913f201af2739f51500c1f844a4de1
Max A <corp.workMA@gmail.com>
2025-05-10 23:05:08 +0300

    remove additional packages

--- packages/ui/src/buttons/Button.tsx
+++ packages/ui/src/buttons/Button.tsx
@@ -2,7 +2,6 @@ import {
   Button as ChakraButton,
   type ButtonProps as ChakraButtonProps,
 } from "@chakra-ui/react";
-import type { ResponsiveValue } from "@chakra-ui/system";
 
 type ButtonVariant = "contained" | "outlined" | "text";
 type ButtonSize = "small" | "medium" | "large";
@@ -22,10 +21,33 @@ const variantMap: Record<ButtonVariant, ChakraButtonProps["variant"]> = {
 
 type ButtonProps = Omit<ChakraButtonProps, "size" | "variant"> & {
   type?: "primary" | "secondary" | "error";
-  size?: ResponsiveValue<ButtonSize>;
-  variant?: ResponsiveValue<ButtonVariant>;
+  size?: ButtonSize;
+  variant?: ButtonVariant;
 };
 
+function mapResponsiveValue<TFrom extends string, TTo>(
+  value: TFrom | undefined,
+  map: Record<TFrom, TTo>
+): TTo | undefined {
+  if (!value) return undefined;
+  if (typeof value === "string") return map[value as TFrom];
+  if (Array.isArray(value))
+    return value.map((v) => (v ? map[v as TFrom] : null));
+  if (typeof value === "object") {
+    const result: any = {};
+    for (const key in value) {
+      result[key] = map[value[key] as TFrom];
+    }
+    return result;
+  }
+  return undefined;
+}
+
 export const Button = (props: ButtonProps) => {
-  return <ChakraButton {...props} />;
+  const { size, variant, ...rest } = props;
+
+  const currentSize = mapResponsiveValue(size, sizeMap);
+  const currentVariant = mapResponsiveValue(variant, variantMap);
+
+  return <ChakraButton size={currentSize} variant={currentVariant} {...rest} />;
 };
